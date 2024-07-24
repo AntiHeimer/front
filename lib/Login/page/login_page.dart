19970f44ca76f6d3b4218cff372cc7ce46f0b2d4
강민재 <minijae011030@naver.com>
2024-07-25 00:28:22 +0900

    login page css

--- lib/Login/page/login_page.dart
+++ lib/Login/page/login_page.dart
@@ -12,25 +12,44 @@ class _LoaginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           children: <Widget>[
-            Padding(padding: EdgeInsets.fromLTRB(0, 100, 0, 50)),
-            Text(
+            const Padding(padding: EdgeInsets.fromLTRB(0, 100, 0, 50)),
+            const Text(
               'AntiHeimer',
               style: TextStyle(
-                fontSize: 40,
+                fontSize: 35,
                 fontWeight: FontWeight.w900,
                 letterSpacing: -1,
               ),
             ),
-            Padding(padding: EdgeInsets.only(bottom: 40)),
-            TextFieldWidget(hintText: '아이디를 입력하세요'),
-            Padding(padding: EdgeInsets.only(bottom: 14)),
-            TextFieldWidget(hintText: '비밀번호를 입력하세요'),
-            Padding(padding: EdgeInsets.only(bottom: 28)),
-            MainButton(text: '로그인'),
+            const Padding(padding: EdgeInsets.only(bottom: 40)),
+            const TextFieldWidget(hintText: '아이디를 입력하세요'),
+            const Padding(padding: EdgeInsets.only(bottom: 14)),
+            const TextFieldWidget(hintText: '비밀번호를 입력하세요'),
+            const Padding(padding: EdgeInsets.only(bottom: 28)),
+            const MainButton(text: '로그인'),
+            const Padding(padding: EdgeInsets.only(bottom: 20)),
+            InkWell(
+              onTap: () {},
+              child: const Text(
+                '회원가입',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromRGBO(52, 52, 52, 1),
+                ),
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 40)),
+            const SocialLogin(),
+            const Padding(padding: EdgeInsets.only(bottom: 25)),
+            Image.asset(
+              'assets/images/kakao_login_large_wide.png',
+              fit: BoxFit.contain,
+              height: 45,
+            ),
           ],
         ),
       ),
@@ -92,9 +111,40 @@ class MainButton extends StatelessWidget {
             backgroundColor: Colors.black, foregroundColor: Colors.white),
         child: Text(
           text,
-          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w200),
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
         ),
       ),
+    );
+  }
+}
+
+class SocialLogin extends StatelessWidget {
+  const SocialLogin({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          height: 1,
+          width: 100,
+          color: const Color.fromRGBO(113, 113, 113, 1),
+        ),
+        const Padding(padding: EdgeInsets.only(right: 15)),
+        const Text(
+          '간편로그인',
+          style: TextStyle(color: Color.fromRGBO(52, 52, 52, 1)),
+        ),
+        const Padding(padding: EdgeInsets.only(left: 15)),
+        Container(
+          height: 1,
+          width: 100,
+          color: const Color.fromRGBO(113, 113, 113, 1),
+        ),
+      ],
     );
   }
 }
