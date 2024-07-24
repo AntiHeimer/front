f13d5904ceee879b46f704439251850f9bdc5dfa
강민재 <minijae011030@naver.com>
2024-07-24 20:34:02 +0900

    navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:anti_heimer/Account/page/account_page.dart';
 import 'package:anti_heimer/Home/page/home_page.dart';
 import 'package:anti_heimer/Location/page/location_page.dart';
-import 'package:anti_heimer/Main/component/bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -15,6 +14,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    LocationPage(),
+    HomePage(),
+    AccountPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,25 +38,39 @@ class _MyAppState extends State<MyApp> {
         colorScheme: const ColorScheme.light(
             primary: Colors.white60, secondary: Colors.black),
       ),
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              Container(
-                child: LocationPage(),
-              ),
-              Container(
-                child: HomePage(),
-              ),
-              Container(
-                child: AccountPage(),
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomBar(),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          backgroundColor: const Color.fromRGBO(245, 245, 245, 1),
+          selectedItemColor: Colors.black,
+          selectedFontSize: 10,
+          unselectedItemColor: const Color.fromRGBO(113, 113, 113, 1),
+          unselectedFontSize: 10,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.location_on_outlined,
+                  size: 35,
+                ),
+                label: 'Location'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                  size: 35,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.account_circle_outlined,
+                  size: 35,
+                ),
+                label: 'Account'),
+          ],
         ),
       ),
     );
